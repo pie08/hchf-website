dd5f87af68be6b95929b6686409891e39a98ad74
Tyrus Berggren <tyrusbrianberggren@gmail.com>
2023-06-27 00:03:56 -0400

    navigation animation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,46 @@
 import Button from "./Button";
 import styles from "../sass/layout/Navigation.module.scss";
 import { NavLink } from "react-router-dom";
+import { useEffect, useRef } from "react";
 
 export default function Navigation() {
-  return (
-    <header className={styles.navigation}>
-      <NavLink to="/home" className={styles.navigation__logoWrapper}>
-        <img
-          src="assets/images/logos/HCHF-logo-black.png"
-          className={styles.navLogo}
-          alt="Half century health and fitness logo"
-        />
-      </NavLink>
+  const navRef = useRef();
 
-      <nav>
+  // Navigation fade on hover animation
+  function handleNavHover(e, opacity) {
+    if (e.target.nodeName !== "A" && e.target.nodeName !== "BUTTON") return;
+
+    const link = e.target.closest("li");
+    const siblings = e.target.closest("ul").querySelectorAll("li");
+    const logo = navRef.current.querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+
+  useEffect(
+    function () {
+      navRef.current.addEventListener("mouseover", (e) =>
+        handleNavHover(e, 0.5)
+      );
+      navRef.current.addEventListener("mouseout", (e) => handleNavHover(e, 1));
+    },
+    [navRef]
+  );
+
+  return (
+    <header>
+      <nav className={styles.navigation} ref={navRef}>
+        <NavLink to="/home" className={styles.navigation__logoWrapper}>
+          <img
+            src="assets/images/logos/HCHF-logo-black.png"
+            className={styles.navLogo}
+            alt="Half century health and fitness logo"
+          />
+        </NavLink>
+
         <ul className={styles.navigation__list}>
           <li>
             <NavLink to="/home">Home</NavLink>
