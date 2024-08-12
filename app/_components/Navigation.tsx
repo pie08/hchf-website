bce77d37e36f01c34760d41c6d7c1842c143eabf
Tyrus Berggren <tyrusbrianberggren@gmail.com>
2024-08-12 01:07:48 -0400

    navigation

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,26 +1,194 @@
+"use client";
+
 import { styled } from "@linaria/react";
-import { FC } from "react";
+import React, { FC, ReactNode, useState } from "react";
 import { Container } from "./Container";
 import Logo from "./Logo";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { PiCaretDown } from "react-icons/pi";
 
-const Header = styled.header``;
+const Header = styled.header`
+  padding: 0 3.2rem;
+`;
 
-const StyledNavigation = styled.div`
+const StyledNavigation = styled(Container)`
   display: flex;
   justify-content: space-between;
-  padding: 1.6rem 4.8rem;
-  background-color: red;
+  align-items: center;
+  padding: 1.6rem 0;
+
+  &:hover nav ul li {
+    opacity: 50%;
+  }
+`;
+
+const NavList = styled.ul`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  gap: 2.4rem;
+
+  & li {
+    transition: all 0.2s;
+
+    &:hover {
+      opacity: 1 !important;
+    }
+  }
+`;
+
+const NavLink = styled.a`
+  font-size: 1.8rem;
+  font-weight: 600;
+  line-height: 1.25;
+
+  box-shadow: inset 0 -2px 0 0 transparent;
+  transition: all 0.2s;
+
+  &[data-active="active"] {
+    box-shadow: inset 0 -2px 0 0 var(--color-primary-600);
+  }
 `;
 
 interface NavigationProps {}
 
 const Navigation: FC<NavigationProps> = ({}) => {
+  const pathname = usePathname();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   return (
     <Header>
       <StyledNavigation>
         <Logo width={9.6} />
+
+        <nav>
+          <NavList>
+            <li>
+              <NavLink href="/" data-active={pathname === "/" ? "active" : ""}>
+                Home
+              </NavLink>
+            </li>
+
+            <Dropdown
+              text="Services"
+              open={dropdownOpen}
+              setOpen={setDropdownOpen}
+            >
+              <NavLink href="/adult-training">Adult Training</NavLink>
+              <NavLink href="/adult-training">Post Surgical Training</NavLink>
+              <NavLink href="/adult-training">Baseball & Softball</NavLink>
+              <NavLink href="/adult-training">Online Consulting</NavLink>
+            </Dropdown>
+
+            <li>
+              <NavLink
+                href="/aging"
+                data-active={pathname === "/aging" ? "active" : ""}
+              >
+                Aging & Hormones
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                href="/profiles"
+                data-active={pathname === "/profiles" ? "active" : ""}
+              >
+                Client Profiles
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                href="testimonials"
+                data-active={pathname === "/testimonials" ? "active" : ""}
+              >
+                Testimonials
+              </NavLink>
+            </li>
+          </NavList>
+        </nav>
       </StyledNavigation>
     </Header>
+  );
+};
+
+// Drop down
+const DropdownParent = styled.li`
+  position: relative;
+
+  &:hover {
+    .dropdownList {
+      opacity: 1;
+      visibility: visible;
+      translate: 0 0;
+    }
+  }
+
+  & a {
+    display: flex;
+    align-items: center;
+    gap: 0.6rem;
+
+    cursor: pointer;
+  }
+`;
+
+const DropdownWrapper = styled.div`
+  padding-top: 0.8rem;
+  position: absolute;
+  top: 100%;
+  left: 0;
+`;
+
+const DropdownList = styled.ul`
+  width: max-content;
+
+  list-style: none;
+  padding: 1.2rem;
+  background-color: var(--color-gray-50);
+  border-top: 2px solid var(--color-primary-600);
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+
+  /* animation */
+  opacity: 0;
+  visibility: hidden;
+  translate: 0 -1.6rem;
+  transition: all 0.2s;
+
+  /* for mobile navigation */
+  &[data-open="open"] {
+    opacity: 1;
+    visibility: visible;
+    translate: 0 0;
+  }
+`;
+
+interface DropdownProps {
+  children: React.ReactNode;
+  text: string;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Dropdown: FC<DropdownProps> = ({ children, text, open, setOpen }) => {
+  return (
+    <DropdownParent>
+      <NavLink>
+        {text}
+        <PiCaretDown />
+      </NavLink>
+
+      <DropdownWrapper>
+        <DropdownList data-open={open && "open"} className="dropdownList">
+          {React.Children.map(children, (child, i) => (
+            <li key={i}>{child}</li>
+          ))}
+        </DropdownList>
+      </DropdownWrapper>
+    </DropdownParent>
   );
 };
 
--- app/_styles/globalStyles.linaria.global.tsx
+++ app/_styles/globalStyles.linaria.global.tsx
@@ -17,25 +17,32 @@ export const globalStyles = css`
       --color-primary-950: #001433;
       --color-primary-50: #ebf3ff;
 
-      --color-neutral-white: #fff;
-      --color-neutral-50: #f8fafc;
-      --color-neutral-100: #f3f4f6;
-      --color-neutral-200: #e2e8f0;
-      --color-neutral-300: #cbd5e1;
-      --color-neutral-400: #94a3b8;
-      --color-neutral-500: #64748b;
-      --color-neutral-600: #475569;
-      --color-neutral-700: #334155;
-      --color-neutral-800: #1e293b;
-      --color-neutral-900: #0f172a;
-      --color-neutral-950: #020617;
-      --color-neutral-black: #000;
+      --color-gray-white: #fff;
+      --color-gray-50: #f8fafc;
+      --color-gray-100: #f3f4f6;
+      --color-gray-200: #e2e8f0;
+      --color-gray-300: #cbd5e1;
+      --color-gray-400: #94a3b8;
+      --color-gray-500: #64748b;
+      --color-gray-600: #475569;
+      --color-gray-700: #334155;
+      --color-gray-800: #1e293b;
+      --color-gray-900: #0f172a;
+      --color-gray-950: #020617;
+      --color-gray-black: #000;
 
       --color-red-100: #fee2e2;
       --color-red-400: #f87171;
 
       --color-green-100: #dcfce7;
       --color-green-400: #4ade80;
+    }
+
+    *,
+    *::before,
+    *::after {
+      margin: 0;
+      padding: 0;
     }
 
     html {
@@ -47,8 +54,23 @@ export const globalStyles = css`
       font-size: 1.6rem;
       line-height: 1.5;
       font-weight: 400;
+      line-height: 1.6;
       color: var(--color-neutral-800);
       min-height: 100vh;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5 {
+      font-weight: 600;
+      line-height: 1.25;
+    }
+
+    a {
+      text-decoration: none;
+      color: inherit;
     }
   }
 `;
